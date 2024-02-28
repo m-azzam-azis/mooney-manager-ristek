8ea746121efb322ecbc3bdc3e90005b6d8fd5e57
m.azzam <m.azzam.azis@gmail.com>
2024-02-28 19:30:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", // Include the root HTML file
+    "./pages/**/*.{html,js}",], // Include files within the "pages" folder],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

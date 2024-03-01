d434a085a0286c408904961a92d710fffafd07e1
m.azzam <m.azzam.azis@gmail.com>
2024-03-01 16:34:28 +0700

    Finished design for now starting js

---
+++ app.js
@@ -0,0 +1,19 @@
+const incomeId = document.querySelector("#income");
+const expenseId = document.querySelector("#expense");
+const budgetId = document.querySelector("#budget");
+const editBudget = document.querySelector("#editBudget");
+const newRecord = document.querySelector("#new-record");
+
+let totalIncome = 0;
+let totalExpense = 0;
+let totalBudget = 0;
+
+
+
+function displayIncome(newExpense = 0) {
+  totalIncome == 0 ? incomeId.textContent = "-" : totalBudget;
+}
+
+function displayExpense(newincome = 0) {
+
+}
